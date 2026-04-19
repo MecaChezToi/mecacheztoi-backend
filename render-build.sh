03e0ec0daca2f434d63b89db4031f6814a8f163e
Stephane <schemansteph@gmail.com>
2026-04-19 17:21:26 +0200

    fix puppeteer

--- render-build.sh
+++ render-build.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-echo "INSTALL MATCHING CHROME FOR PUPPETEER"
+echo "INSTALL CHROME IN PROJECT DIR"
+export PUPPETEER_CACHE_DIR=/opt/render/project/.cache/puppeteer
 npx --yes puppeteer@latest browsers install chrome@121.0.6167.85
+
